9aa0a5d678763a964c5ba7f1d27c3ff05bafd123
Micheletti <73291937+joaoMicheletti@users.noreply.github.com>
2024-08-30 18:18:06 -0300

    register
    creare a connectionwhit database and  register user.

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,14 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import {Login} from './DTO/loginDto.dto';
+import connectio from './database/connectio';
 @Injectable()
 export class AppService {
+  //login user
   async Login(data: Login):  Promise<object>{
     // buscar os dados no database e efetuar o login retornar o token do usuário.
+    console.log(data)
     return {data};
   }
-
+  // register of login
   async Register(data: Login): Promise<object> {
     //verificar a existencia do usuario e se possivél encerir no banco de dados.
+    console.log(data)
+    var confirmation = await connectio('user').where('email', data.email).select('email');
+
+    console.log(confirmation.length);
+    if(confirmation.length > 0 ){
+      return {resp: "Usuário já cadastrado."};
+    } else {
+      var Data = {email: data.email, pass: data.senha, token: data.email+data.senha};
+      var insert = await connectio('user').insert(Data);
+      console.log(insert);
+      if(insert.length > 0) {
+        return {resp: "Cadastrado com sucesso!"}
+      }
+    }
+
+
     return{data};
   }
 }
