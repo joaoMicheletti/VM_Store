d24a15f05337467c5c33666925bebb2427ad7d1d
Micheletti <73291937+joaoMicheletti@users.noreply.github.com>
2024-09-02 17:33:05 -0300

    Login and acess
    foi finalizada a comunicação com o servidor na página de login.
    
    adicionada a verificação na página do carrinho, para garantir que apenas usuários credenciados tenham acesso a essa página.

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -7,8 +7,22 @@ export class AppService {
   async Login(data: Login):  Promise<object>{
     // buscar os dados no database e efetuar o login retornar o token do usuário.
     console.log(data)
-    return {data};
-  }
+    var email = await connectio('user').where('email', data.email).select("*");
+    console.log(email.length);
+    if(email.length === 0){
+      return {resp: 'Usuário não encontrado.'};
+    } else if( email.length > 0){
+      var cSenha = await connectio('user').where('email', data.email).select('pass');
+      console.log(cSenha[0].pass);
+      if(cSenha[0].pass === data.senha){
+        var token = await connectio('user').where('email', data.email).select('token');
+        console.log(token)
+        return {resp: `${token[0].token}`};
+      } else {
+        return {resp: "Usuário ou senha invalidos."};
+      };
+    };
+  };
   // register of login
   async Register(data: Login): Promise<object> {
     //verificar a existencia do usuario e se possivél encerir no banco de dados.
